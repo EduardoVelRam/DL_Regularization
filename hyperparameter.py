9ebad5d564976f19f94f34da6b0ecaf14b7bf19d
EduardoVelRam <velramedu@gmail.com>
2026-05-19 21:53:41 -0600

    Commit 1905

--- hyperparameter.py
+++ hyperparameter.py
@@ -17,12 +17,28 @@ y_train = y_train_full[:3000]
 x_val = x_train_full[3000:4000]
 y_val = y_train_full[3000:4000]
 
+# Flatten images for a simple dense network
+x_train = x_train.reshape(-1, 784)
+x_val = x_val.reshape(-1, 784)
+x_test = x_test.reshape(-1, 784)
 
+# Define a model builder with variable architecture
 
+def build_model(num_layers, units_per_layer=64, learning_rate=0.001):
+    model = models.Sequential()
+    # input layer
+    model.add(layers.Dense(units_per_layer, activation='relu', input_shape=(784,)))
+    # hidden layers
+    for _ in range(num_layers - 1):
+        model.add(layers.Dense(units_per_layer, activation='relu'))
+    # output layer
+    model.add(layers.Dense(10, activation='softmax'))
 
-
-
-
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(optimizer=optimizer,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
 
 
 #
