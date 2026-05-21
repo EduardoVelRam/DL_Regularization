8c7c7fda260876d94cda317e3e57faaaa7aefb33
EduardoVelRam <velramedu@gmail.com>
2026-05-21 17:32:06 -0600

    Training comparison file

---
+++ training_comparison.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers, models, regularizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Load and prepare CIFAR-10 
